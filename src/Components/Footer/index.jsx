417f4a4efe8f2f71a9f5c7e319d368ce7d9361f9
Ryan Bagan <darcknight1980@gmail.com>
2023-05-10 10:00:26 -0700

    styled fonts

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -5,7 +5,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer data-testid='footer'>
-      <p>&copy; Ryan Bagan 2023</p>
+      <p className="purples">&copy; Ryan Bagan 2023</p>
     </footer>
   );
 }
