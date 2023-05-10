4bead60e05f6392471176784615c5c79004b6dc9
Ryan Bagan <darcknight1980@gmail.com>
2023-05-10 15:19:14 -0700

    reducer

---
+++ src/Components/History/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const History = ({ history, showHistory }) => {
+  return (
+    // <>
+    <section id="history">
+      <h2>History</h2>
+      {history.map((entry, index) => (
+        <button key={`entry-${index}`} onClick={() => showHistory(entry)}>
+          {entry.request.method} : {entry.request.url}
+        </button>
+      ))}
+    </section>
+    // {/* </> */}
+  );
+};
+
+export default History;
