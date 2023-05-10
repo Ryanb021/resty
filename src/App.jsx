0269b37e198da785c9735418d1ec6604656d3478
Ryan Bagan <darcknight1980@gmail.com>
2023-05-10 15:54:25 -0700

    reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 import './App.scss';
@@ -11,115 +11,146 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
+import History from './Components/History';
 
-const App =() => {
-  let [data, setData] = useState(null);
-  let [requestParams, setRequestParams] = useState({});
-  const [loading, setLoading] = useState(false);
+const initialState = {
+  data: null,
+  requestParams: {},
+  history: [],
+};
+
+const requestReducer = (state = initialState, action) => {
+  const { payload, type } = action;
+
+  switch (type) {
+    case 'START_REQUEST':
+      return {
+        ...state,
+        requestParams: payload,
+      };
+    case 'FINISH_REQUEST':
+      return {
+        ...state,
+        data: payload,
+        history: [
+          ...state.history,
+          { request: state.requestParams, data: payload },
+        ],
+      };
+    case 'SHOW_HISTORY':
+      return {
+        ...state,
+        data: payload.data,
+        request: payload.request,
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(requestReducer, initialState);
 
   const callApi = (requestParams) => {
-    // mock output
-    setTimeout(() => {
-      const data = {
-        count: 2,
-        results: [
-          { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-          { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-        ],
-      }
-      setData(data);
-      setRequestParams(requestParams);
-      setLoading(false);
-    }, 1000);
-  }
+    const action = {
+      type: 'START_REQUEST',
+      payload: requestParams,
+    };
+
+    dispatch(action);
+  };
+
+  const showHistory = (entry) => {
+    const action = {
+      type: 'SHOW_HISTORY',
+      payload: entry,
+    };
+    dispatch(action);
+  };
 
   useEffect(() => {
     const getData = async () => {
-      if(requestParams.method && requestParams.url) {
-        setLoading(true);
-        const response = await axios(requestParams);
-        const data = response.data;
-        setData(data);
-        setLoading(false);
+      if (state.requestParams.url) {
+        const response = await axios({
+          method: state.requestParams.method,
+          url: state.requestParams.url,
+        });
+        const action = {
+          type: 'FINISH_REQUEST',
+          payload: response.data,
+        };
+        dispatch(action);
       }
-    }
-
+    };
     getData();
-  }, [requestParams]);
+  }, [state.requestParams]);
 
   return (
     <>
       <Header />
-      <div>Request Method: {requestParams.method ? requestParams.method.toUpperCase() : ''}</div>
-      <div>URL: {requestParams.url}</div>
-      
+      <div>Request Method: {state.requestParams.method}</div>
+      <div>URL: {state.requestParams.url}</div>
       <Form handleApiCall={callApi} />
-      <div>Data: {requestParams.data}</div>
-      <Results data={data} loading={loading} />
+      {state.data ? (
+        <History history={state.history} showHistory={showHistory} />
+      ) : null}
+      {/* <History history={state.history} showHistory={showHistory} /> */}
+      {state.data ? 
+      <Results data={state.data} /> : null}
       <Footer />
     </>
-  )
+  );
 };
 
 export default App;
 
 // const App =() => {
-
 //   let [data, setData] = useState(null);
-//    let [requestParams, setRequestParams] = useState({});
- 
-//    const callApi = (requestParams) => {
-//     setData(data);
-// setRequestParams(requestParams);
-//   }
+//   let [requestParams, setRequestParams] = useState({});
+//   const [loading, setLoading] = useState(false);
 
-
-//     return (
-//       <>
-     
-//         <Header />
-//         <div className='method'>Request Method: {requestParams.method}</div>
-//         <div className='method'>URL: {requestParams.url}</div>
-//         <Form handleApiCall={callApi} />
-//         <Results data={data} />
-//         <Footer />
-//       </>
-//     );  
-//   }
-
-
-// class App extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: null,
-//       requestParams: {},
-//     };
-//   }
-
-//   callApi = (requestParams) => {
+//   const callApi = (requestParams) => {
 //     // mock output
-//     const data = {
-//       count: 2,
-//       results: [
-//         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-//         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-//       ],
-//     };
-//     this.setState({data, requestParams});
+//     setTimeout(() => {
+//       const data = {
+//         count: 2,
+//         results: [
+//           { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+//           { name: 'fake thing 2', url: 'http://fakethings.com/2' },
+//         ],
+//       }
+//       setData(data);
+//       setRequestParams(requestParams);
+//       setLoading(false);
+//     }, 1000);
 //   }
 
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <Header />
-//         <div>Request Method: {this.state.requestParams.method}</div>
-//         <div>URL: {this.state.requestParams.url}</div>
-//         <Form handleApiCall={this.callApi} />
-//         <Results data={this.state.data} />
-//         <Footer />
-//       </React.Fragment>
-//     );
-//   }
-// }
+//   useEffect(() => {
+//     const getData = async () => {
+//       if(requestParams.method && requestParams.url) {
+//         setLoading(true);
+//         const response = await axios(requestParams);
+//         const data = response.data;
+//         setData(data);
+//         setLoading(false);
+//       }
+//     }
+
+//     getData();
+//   }, [requestParams]);
+
+//   return (
+//     <>
+//       <Header />
+//       <div>Request Method: {requestParams.method ? requestParams.method.toUpperCase() : ''}</div>
+//       <div>URL: {requestParams.url}</div>
+      
+//       <Form handleApiCall={callApi} />
+//       <div>Data: {requestParams.data}</div>
+//       <Results data={data} loading={loading} />
+//       <Footer />
+//     </>
+//   )
+// };
+
+// export default App;
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -4,48 +4,69 @@ import { useState } from 'react';
 import './Form.scss';
 
 const Form = (props) => {
-  const[method, setMethod] = useState('get');
-  const[url, setUrl] = useState('');
-  const[data, setData] = useState('');
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('');
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
       method,
       url,
-      data,
     };
-
     props.handleApiCall(formData);
-  }
+  };
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <label>
-        <span>URL: </span>
-        <input data-testid="form-url" onChange={(e) => setUrl(e.target.value)} name='url' type='text' />
-        <button data-testid="button" type="submit">GO!</button>
-      </label>
-
-      <label className="methods">
-        <span data-testid="get-span" onClick={(e) => setMethod(e.target.id)}>GET</span>
-        <span data-testid="post-span" onClick={(e) => setMethod(e.target.id)}>POST</span>
-        <span data-testid="put-span" onClick={(e) => setMethod(e.target.id)}>PUT</span>
-        <span data-testid="patch-span" onClick={(e) => setMethod(e.target.id)}>PATCH</span>
-        <span data-testid="delete-span" onClick={(e) => setMethod(e.target.id)}>DELETE</span>
-      </label>
-
-      <label id="textarea">
-        POST / PUT Request JSON Data:
-        <textarea data-testid="form-textarea" onChange={(e)=> setData(e.target.value)} rows="5" cols="50"></textarea>
-      </label>
-
-     
-    </form>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span data-testid='get-span'>URL: </span>
+          <input
+            name='url'
+            data-testid='url-input'
+            type='text'
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button type='submit' data-testid='submit-button'>
+            GO!
+          </button>
+        </label>
+        <label className='methods'>
+          <span
+            className={method === 'GET' ? 'active' : 'inactive'}
+            id='get'
+            onClick={() => setMethod('GET')}>
+            GET
+          </span>
+          <span
+            className={method === 'POST' ? 'active' : 'inactive'}
+            id='post'
+            onClick={() => setMethod('POST')}>
+            POST
+          </span>
+          <span
+            className={method === 'PUT' ? 'active' : 'inactive'}
+            id='put'
+            onClick={() => setMethod('PUT')}>
+            PUT
+          </span>
+          <span
+            className={method === 'PATCH' ? 'active' : 'inactive'}
+            id='put'
+            onClick={() => setMethod('PATCH')}>
+            PATCH
+          </span>
+          <span
+            className={method === 'DELETE' ? 'active' : 'inactive'}
+            id='delete'
+            onClick={() => setMethod('DELETE')}>
+            DELETE
+          </span>
+        </label>
+      </form>
     </>
   );
-}
+};
 
 export default Form;
 // const Form = props => {
